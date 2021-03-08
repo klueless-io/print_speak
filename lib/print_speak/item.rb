616ae9035e8e8401ef8618ded21a14c8913bc08f
klueless-io <david@ideasmen.com.au>
2021-03-09 08:35:07 +1100

    add item attribute validation

--- lib/print_speak/item.rb
+++ lib/print_speak/item.rb
@@ -3,6 +3,23 @@
 module PrintSpeak
   # Represents a purchase item on a receipt
   class Item
-    attr_accessor :quantity, :product, :price
+    attr_accessor :quantity, :product, :price, :category
+
+    def initialize(**opts)
+      # Simulate strong_params
+      opts = opts.slice(:quantity, :product, :price, :category)
+
+      # Set attributes
+      opts.each { |k, v| instance_variable_set("@#{k}", v) }
+
+      # mutable and raise if not set correctly
+      guards
+    end
+
+    def guards
+      raise PrintSpeak::Error, 'Quantity required' if quantity.nil? || !quantity.is_a?(Integer)
+      raise PrintSpeak::Error, 'Price required' if price.nil? || !(price.is_a?(Integer) || price.is_a?(Float))
+      raise PrintSpeak::Error, 'Product required' if product.nil?
+    end
   end
 end
--- spec/print_speak/item_spec.rb
+++ spec/print_speak/item_spec.rb
@@ -1,11 +1,45 @@
 # frozen_string_literal: true
 
 RSpec.describe PrintSpeak::Item do
-  let(:instance) { described_class.new }
+  let(:instance) { described_class.new(opts) }
+  let(:opts) { {} }
+  let(:product) { { quantity: 1, product: 'general-product', price: 100.0 } }
+  let(:book) { { quantity: 1, product: 'some-book', price: 10.0, category: :book } }
+  let(:food) { { quantity: 1, product: 'some-food', price: 20.0, category: :food } }
+  let(:medical) { { quantity: 1, product: 'some-medical-supply', price: 30.0, category: :medical } }
 
   describe 'initialize' do
     subject { instance }
 
-    it { is_expected.not_to be_nil }
+    context 'no options' do
+      it { expect { subject }.to raise_error(PrintSpeak::Error) }
+    end
+
+    context 'with invalid options' do
+      let(:opts) { product }
+      context '.quantity' do
+        before { product.delete(:quantity) }
+        it { expect { subject }.to raise_error(PrintSpeak::Error, 'Quantity required') }
+      end
+      context '.price' do
+        before { product.delete(:price) }
+        it { expect { subject }.to raise_error(PrintSpeak::Error, 'Price required') }
+      end
+      context '.product' do
+        before { product.delete(:product) }
+        it { expect { subject }.to raise_error(PrintSpeak::Error, 'Product required') }
+      end
+    end
+
+    context 'with required parameters' do
+      let(:opts) { product }
+      it do
+        is_expected
+          .to  have_attributes(quantity: 1,
+                               product: 'general-product',
+                               price: 100,
+                               category: be_nil)
+      end
+    end
   end
 end
