aedf1b7295fe4eccb893f1dc3a4945ca9e69fdc9
klueless-io <david@ideasmen.com.au>
2021-03-09 08:06:44 +1100

    add github actions

--- spec/print_speak/speak_spec.rb
+++ spec/print_speak/speak_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-RSpec.describe Print::Speak do
+RSpec.describe PrintSpeak do
   it 'has a version number' do
-    expect(Print::Speak::VERSION).not_to be nil
+    expect(PrintSpeak::VERSION).not_to be nil
   end
 
   it 'has a standard error' do
-    expect { raise Print::Speak::Error, 'some message' }
+    expect { raise PrintSpeak::Error, 'some message' }
       .to raise_error('some message')
   end
 end
