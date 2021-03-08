af66bd89b92f8c0416ec34bd2d75ff897678c7a7
klueless-io <david@ideasmen.com.au>
2021-03-09 07:46:55 +1100

    first commit

---
+++ lib/print_speak.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "print_speak/version"
+
+module PrintSpeak
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/print_speak/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PrintSpeak
+  VERSION = "0.1.0"
+end
