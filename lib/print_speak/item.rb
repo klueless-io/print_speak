9133fc30108412766b5314f67907d9a877a5013c
klueless-io <david@ideasmen.com.au>
2021-03-09 08:49:38 +1100

    add import duty and fix cop

--- lib/print_speak/item.rb
+++ lib/print_speak/item.rb
@@ -19,21 +19,29 @@ module PrintSpeak
       guards
 
       calculate_sales_tax
+      calculate_import_duty
     end
 
     private
 
+    # rubocop:disable Metrics/AbcSize
     def guards
       raise PrintSpeak::Error, 'Quantity required' if quantity.nil? || !quantity.is_a?(Integer)
       raise PrintSpeak::Error, 'Price required' if price.nil? || !(price.is_a?(Integer) || price.is_a?(Float))
       raise PrintSpeak::Error, 'Product required' if product.nil?
     end
+    # rubocop:enable Metrics/AbcSize
 
     # Basic sales tax is applicable at a rate of 10% on all goods
     #   except books, food, and medical products that are exempt.
     def calculate_sales_tax
       @sales_tax = 0
       @sales_tax = (price * 0.1) unless %i[book food medical].include?(category)
+    end
+
+    # Import duty is an additional sales tax applicable on all imported goods at a rate of 5%, with no exemptions.
+    def calculate_import_duty
+      @import_duty = imported ? (price * 0.05) : 0.0
     end
   end
 end
