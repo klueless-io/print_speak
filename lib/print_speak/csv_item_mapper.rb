b5ada2ef6fbd0bccc6a09a774ee283dfe258fbcb
klueless-io <david@ideasmen.com.au>
2021-03-09 11:16:07 +1100

    add csv_item_mapper implementation and spec

--- lib/print_speak/csv_item_mapper.rb
+++ lib/print_speak/csv_item_mapper.rb
@@ -2,10 +2,23 @@
 
 module PrintSpeak
   # Map items from CSV reader to type safe items
-  class CsvItemMap
-    def self.map(_csv_reader)
-      # xxx
-      []
+  class CsvItemMapper
+    # Convert CSV to array of Hash using opinionated headings (downcase, symbol)
+    #
+    # example:
+    #   [{"Quantity"=>1, "Product"=>"book", "Price"=>12.49}, {"Quantity"=>1, "Product"=>"music cd", "Price"=>14.99}, {"Quantity"=>1, "Product"=>"chocolate bar", "Price"=>0.85}]
+    # becomes
+    #   [{:quantity=>1, :product=>"book", :price=>12.49}, {:quantity=>1, :product=>"music cd", :price=>14.99}, {:quantity=>1, :product=>"chocolate bar", :price=>0.85}]
+    #
+    def self.map(csv_reader)
+      csv_reader.rows.map do |row|
+        csv_reader
+          .headings
+          .map(&:downcase)
+          .map(&:to_sym)
+          .zip(row)
+          .to_h
+      end
     end
   end
 end
